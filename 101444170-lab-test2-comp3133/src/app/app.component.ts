f2fb5d3ad213f5dd6c8689a3dfec5d5486bf71f2
Nazneen Akter Nitu <nazanitu@gmail.com>
2025-04-02 19:38:23 -0400

    Updated feature X or fixed bug Y

--- 101444170-lab-test2-comp3133/src/app/app.component.ts
+++ 101444170-lab-test2-comp3133/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [],  // <-- You can either remove this line or leave it empty if you don't need routing
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrl: './app.component.css'
 })
-
 export class AppComponent {
   title = '101444170-lab-test2-comp3133';
 }
--- 101444170-lab-test2-comp3133/src/app/app.config.ts
+++ 101444170-lab-test2-comp3133/src/app/app.config.ts
@@ -1,27 +1,8 @@
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideHttpClient(),
-    provideAnimations(),
-    importProvidersFrom(
-      MatToolbarModule,
-      MatCardModule,
-      MatButtonModule,
-      MatSelectModule,
-      MatFormFieldModule,
-      MatInputModule,
-      MatGridListModule
-    )
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
